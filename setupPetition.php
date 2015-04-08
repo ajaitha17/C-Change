e89cd12706aab9bc323243a964c4e7096eedcb37
ajaitha17 <ajaitha17@cmc.edu>
2015-04-07 19:17:01 -0700

    Create setupPetition.php

---
+++ setupPetition.php
@@ -0,0 +1,68 @@
+<?php
+	
+	require_once "login.php";
+	$conn = new mysqli("localhost","root","");
+	
+
+	$query = "DROP DATABASE IF EXISTS petition";
+ 	$result = $conn->query($query);
+ 	if (!$result) {
+		echo "Connection Error";
+	}
+	
+	$result = $conn->query("CREATE DATABASE petition");
+	if (!$result) {
+		echo "Connection Error";
+	}
+
+	$query = "USE petition";
+	$result = $conn->query($query);
+	if (!$result) {
+		echo "Connection Error switch";
+	}
+
+	$query = "CREATE TABLE user(
+	user_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name text NOT NULL,
+	email text NOT NULL,
+	username text NOT NULL,
+	password text NOT NULL,
+	class int NOT NULL,
+	dorm text NOT NULL) ENGINE=InnoDB";
+	$result = $conn->query($query);
+	if(!$result){
+		echo "Didn't create user table successfully.";
+	}
+
+	$query = "CREATE TABLE petitions(
+	user_id int NOT NULL,
+	title text NOT NULL,
+	body text NOT NULL,
+	pet_id int NOT NULL,
+	num_backers int NOT NULL,
+	time_submitted timestamp NOT NULL,
+	PRIMARY KEY (pet_id)) ENGINE=InnoDB";
+	$result = $conn->query($query);
+	if(!$result){
+		echo "Didn't create petitions table successfully.";
+	}
+
+	$query = "CREATE TABLE backed(
+	pet_id int NOT NULL,
+	backer_ids text NOT NULL) ENGINE=InnoDB";
+	$result = $conn->query($query);
+	if(!$result){
+		echo "Didn't create petitions backed successfully. ";
+	}
+
+	$query = "CREATE TABLE saved(
+	user_id int NOT NULL,
+	saved_ids int NOT NULL) ENGINE=InnoDB";
+	$result = $conn->query($query);
+	if(!$result){
+		echo "Didn't create saved table successfully. ";
+	}
+
+?>
+
+<a href="homePage.php"> Click here to get going. </a>
