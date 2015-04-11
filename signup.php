afec28cf2eb3ae88d9a555934f919c33f7674747
ajaitha17 <ajaitha17@cmc.edu>
2015-04-11 07:34:07 -0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -3,9 +3,8 @@
 <title> Welcome!</title>
 </head>
 <body>
-<h1> Your info </h1>
 <?php 
-	include "functions.php";
+	require "functions.php";
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$usrname = $_POST['usrname'];
@@ -13,19 +12,29 @@
 	$dorm = $_POST['dorm'];
 	$pass1 = $_POST['pass1'];
 	$pass2 = $_POST['pass2'];
-
-	echo $name . " " . $email . " " . $usrname . " " . $class . " " . $dorm . " " . $pass1 . " " . $pass2."<br>";
+	session_start();
+	$_SESSION['un'] = $usrname;
+	$_SESSION['pw'] = $pass1;
+	//var_dump($_SESSION);
+	// echo $name . " " . $email . " " . $usrname . " " . $class . " " . $dorm . " " . $pass1 . " " . $pass2."<br>";
 	
 	//insertuser($name, $email, $usrname, $pass1, $class, $dorm, $pass2);
-	
+?>
+<h1> Welcome <?php echo $name?> </h1>
+
+<?php	
 	$conn = new mysqli("localhost", "root", "", "petition");
     if($pass1 == $pass2) {
-    	if(checkEmail($email) && checkUsername($usrname)) {
+    	if(checkEmail($email) && uniqueUser($usrname)) {
     		$query = "INSERT INTO user VALUES ('NULL', '$name', '$email', '$usrname', '$pass1', '$class', '$dorm')";
     		$result = $conn->query($query);
     		if(!$result) {
     			echo "User insert error";
+    			echo "<a href='homePage.php'>Home</a>";
+    			die();
     		}
+			
+    		echo "<a href='feed.php'>Click here to get started.</a>";
     	}
     }
 	
@@ -43,4 +52,3 @@
 //   }
 ?>
 </body>
-
