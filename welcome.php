90419a59dd20b154479c5e808d56e40312b45e52
ajaitha17 <ajaitha17@cmc.edu>
2015-11-22 15:49:28 -0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -3,23 +3,58 @@
 	require_once "functions.php";
 	if(isset($_POST['username']) && isset($_POST['password'])){
 		$username = sanitizeString($_POST['username']);
-		$password = sanitizeString($_POST['password']);
-		$result = findUser($username, $password);
-		$rowNum= mysqli_num_rows($result);
-		//check number of rows instead
-		if($rowNum > 0){
-			session_start();
-			$_SESSION['un'] = $username;
-			$_SESSION['pw'] = $password;
-			//var_dump($_SESSION);
-			include "feed.php";
-		} else {
-			include "homePage.php";
+		$enteredPassword = sanitizeString($_POST['password']);
+		$dbPassword = getUserPassword($username);
+		if($dbPassword != null){
+			if(verifyPassword($enteredPassword,$dbPassword)){
+				session_start();
+				$_SESSION['un'] = $username;
+				$_SESSION['pw'] = $dbPassword;
+				echo "<h2>Welcome $username</h2><br>";
+				echo "Click <a href = 'feed.php'>Here</a> to see the current Feed of Petitions";
+			}
+			else{
+				echo "Incorrect Username Password combination<br>";
+				echo "Click <a href = 'homePage.php'>Here</a> to try logging in again";
+			}
+		}
+		else{
+			echo "Incorrect Username Password combination<br>";
+			echo "Click <a href = 'homePage.php'>Here</a> to try logging in again";
+
 		}
 	}
-	else{
-		include "homePage.php";	
-	}
+
+
+	// 	$rowNum= mysqli_num_rows($result);
+	// 	//check number of rows instead
+	// 	if($rowNum > 0){
+	// 		session_start();
+	// 		$_SESSION['un'] = $username;
+	// 		$_SESSION['pw'] = $password;
+	// 		//var_dump($_SESSION);
+	// 		//include "feed.php";
+	// 		echo "<h2>Welcome $username</h2><br>";
+	// 		echo "Click <a href = 'feed.php'>Here</a> to see the current Feed of Petitions";
+	// 	} else {
+	// 		//include "homePage.php";
+	// 		echo "Incorrect Username Password combination<br>";
+	// 		echo "Click <a href = 'homePage.php'>Here</a> to try logging in again";
+	// 	}
+	// }
+	// else{
+	// 	//include "homePage.php";	
+	// 	echo "Incorrect Username Password combination<br>";
+	// 	echo "Click <a href = 'homePage.php'>Here</a> to try logging in again";
+	// }
 	
 	
 ?>
+
+<!DOCTYPE html5>
+<html>
+<head>
+	<title>Welcome!</title>
+	<link rel = "stylesheet" type = "text/css" href = "styling.css">
+</head>
+</html>
